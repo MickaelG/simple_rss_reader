21ab25e496621d5e636473af45ab492b0deece3d
MickaëlG <xcsnake@free.fr>
2012-12-30 23:11:44 +0100

    feeds are now stored to be able to use etags and modified parameters to reduce bandwidth

--- rssread.py
+++ rssread.py
@@ -1,16 +1,53 @@
 #!/usr/bin/python3
 
-import feedparser, json, datetime
+import feedparser, json, datetime, pickle, copy
 
 feeds_list = json.load(open("feeds.json",'r'))
 
+feeds_file_name = "tmpfeeds.pickle"
+
 entries=[]
 
-#http://packages.python.org/feedparser/http-etag.html#using-etags-to-reduce-bandwidth
+try:
+	with open(feeds_file_name, "rb") as f:
+		loaded_feeds = pickle.load(f)
+except FileNotFoundError:
+	loaded_feeds = {}
+
 for (url,img) in feeds_list:
-	feed=feedparser.parse(url)
+	print ("Reading feeds from {}".format(url))
+	try:
+		prev_feed = loaded_feeds[url]
+	except KeyError:
+		prev_feed = None
+	if prev_feed:
+		#http://packages.python.org/feedparser/http-etag.html#using-etags-to-reduce-bandwidth
+		try:
+			etag = prev_feed.etag
+		except AttributeError:
+			print ("Warning: feed {} has no etag attribute".format(url))
+			etag = None
+		try:
+			modified = prev_feed.modified
+		except AttributeError:
+			print ("Warning: feed {} has no modified attribute".format(url))
+			modified = None
+		feed = feedparser.parse(url, etag=etag, modified=modified)
+	else:
+		feed = feedparser.parse(url)
+	if feed.status == 304:
+		print ("Information: feed {} returned a 304 status. Keeping old feed".format(url))
+	else:
+		feed['img'] = img
+		loaded_feeds[url] = copy.copy(feed)
+
+with open(feeds_file_name, "wb") as f:
+	pickle.dump(loaded_feeds, f)
+
+for (url, feed) in loaded_feeds.items():
+	print ("Decoding feeds from {}".format(url))
 	for item in feed['items']:
-		item['img']=img
+		item['img']=feed['img']
 	entries.extend( feed['items'] )
 
 sorted_entries=sorted(entries, key=lambda entry: entry["updated_parsed"])
