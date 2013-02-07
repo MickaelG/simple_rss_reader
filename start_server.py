a245450e03e3e8855588badc744fe186299ba1aa
MickaëlG <xcsnake@free.fr>
2013-02-07 22:55:49 +0100

    regenerate the index.html only it is more than one hour old

--- start_server.py
+++ start_server.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
 import bottle
-import os
+import os, datetime
 
 app = bottle.Bottle()
 
 @app.route('/')
+def root():
+	#We regenerate index.html only if last generation is more than 1 hour old
+	last_mod = datetime.datetime.fromtimestamp(os.path.getmtime("index.html"))
+	now = datetime.datetime.now()
+	if ( now - last_mod ).total_seconds() > 60*60:
+		os.system("python3 rssread.py")
+	return bottle.static_file("index.html", root=".")
+
+@app.route('/update')
 def update():
 	os.system("python3 rssread.py")
 	return bottle.static_file("index.html", root=".")
