486732569474d3921cd448a046de118dca03768a
MickaëlG <xcsnake@free.fr>
2013-03-15 09:33:54 +0100

    bug correction if index.html does not exist

--- start_server.py
+++ start_server.py
@@ -8,9 +8,12 @@ app = bottle.Bottle()
 @app.route('/')
 def root():
 	#We regenerate index.html only if last generation is more than 1 hour old
-	last_mod = datetime.datetime.fromtimestamp(os.path.getmtime("index.html"))
+	try:
+		last_mod = datetime.datetime.fromtimestamp(os.path.getmtime("index.html"))
+	except FileNotFoundError:
+		last_mod = None
 	now = datetime.datetime.now()
-	if ( now - last_mod ).total_seconds() > 60*60:
+	if not last_mod or ( now - last_mod ).total_seconds() > 60*60:
 		os.system("python3 rssread.py")
 	return bottle.static_file("index.html", root=".")
 
